9f91586f70394f28b2b190d96dcee12466a695bc
Tereshenko Anatoliy Vitalyevich <anatoliy.tereschenko@rt.ru>
2020-12-14 12:40:52 +0300

    added bonus homework part

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from data import title, departures, tours
 
 app = Flask(__name__)
 
@@ -6,6 +7,31 @@ app = Flask(__name__)
 @app.route('/')
 def render_index():
     return render_template('index.html')
+
+
+@app.route('/data/')
+def render_data():
+    return render_template('data.html',
+                           title=title,
+                           tours=tours)
+
+
+@app.route('/data/departures/<departure>/')
+def render_data_depature(departure):
+    return render_template('data_depature.html',
+                           title=title,
+                           tours=tours,
+                           departure=departure,
+                           departures=departures)
+
+
+@app.route('/data/tours/<int:id>/')
+def render_data_tour(id):
+    return render_template('data_tours.html',
+                           title=title,
+                           id=id,
+                           tours=tours,
+                           departures=departures)
 
 
 @app.route('/departures/<departure>/')
